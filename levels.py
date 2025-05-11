ceb179b9b89dff360ffcc87c19f5a0b541e5f10a
Kostil-ua <kvakulenko@nb21.ukr.education>
2025-05-11 16:05:13 +0300

    Add files via upload

---
+++ levels.py
@@ -0,0 +1,116 @@
+from objects import *
+from settings import *
+
+level1 = [
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "        o                                                              ",
+    "       -                                                               ",
+    "      1                                                                ",
+    "     --                                                                ",
+    "    -                                                                  ",
+    "   -                                                                   ",
+    "  -           2                                                        ",
+    " -           --                                                        ",
+    "    -                                                                   ",
+    "         --          o o                                               ",
+    "  -----            ----                                                ",
+
+
+]
+level2 = [
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+
+
+]
+
+level1_width = len(level1[0])*100
+level1_height = len(level1)*30
+
+level2_width = len(level2[0])*100
+level2_height = len(level2)*30
+
+level_objects = pygame.sprite.Group()
+
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.stste = pygame.Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+    
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+
+def camera_config(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+
+    l = min(0, l)
+    l = max(-(camera.width - W), l)
+    t = max( -(camera.height - H), t)
+    t = min(0, t)
+
+    return pygame.Rect(l, t, w, h)
+
+camera = Camera(camera_config, level1_width, level1_height)
+
+def draw_level(level: list):
+    x = y = 0
+    for row in level:
+        for symbol in row:
+            if symbol == "-":
+                platform = MapObject(x, y, 100, 30, platform_image)
+                level_objects.add(platform)
+                platforms.add(platform)
+            if symbol == "o":
+                coin = MapObject(x, y, 100, 30, coin_image)
+                level_objects.add(coin)
+                coins.add(coin)
+            if symbol == "1":
+                key = MapObject(x, y, 100, 30, key_image)
+                level_objects.add(chest)
+            if symbol == "2":
+                chest = MapObject(x, y, 100, 30, chest_image)
+                level_objects.add(chest)
+            x += 100
+        x = 0
+        y += 30
+
+    return level_objects, key, chest
---
+++ objects.py
@@ -0,0 +1,74 @@
+from settings import *
+ 
+class MapObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.image = pygame.transform.scale(image, (width, height))
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+ 
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+ 
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, speed, images):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.anim_count = 0
+        self.images = images
+        self.image = pygame.transform.scale(self.images[self.anim_count], (width, height))
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+ 
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+ 
+class Player(Sprite):
+    def __init__(self, x, y, width, height, speed, images):
+        super().__init__(x, y, width, height, speed, images)
+ 
+        self.action = 'idle'
+        self.animation = {
+            'idle': list(range(4)),
+            'right': list(range(4, 10)),
+            'left': list(range(10, 16))
+        }
+ 
+        self.is_jump = False
+        self.jump_count = 25
+ 
+        self.fall = 0
+        self.gravity = 2.5
+        self.on_ground = False
+ 
+    def update(self, platforms):
+        pass
+    
+class Button:
+    def __init__(self, x, y, w, h, color, text, txt_size, txt_color):
+        self.w = W
+        self.h = H
+        self.color = color
+
+        self.image = pygame.Surface((w, h))
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        self.text = pygame.font.Font(None, txt_size).render(text, True, txt_color)
+
+    def draw(self, shift_x, shift_y):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        window.blit(self.text, (self.rect.x + shift_x, self.rect.y + shift_y))
